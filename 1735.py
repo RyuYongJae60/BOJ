749fb0ecad5d0fbf6bb371859b0911909cd5bf37
Ryu <112682983+RyuYongJae60@users.noreply.github.com>
2023-05-25 23:42:09 +0900

    Create 1735.py

---
+++ 1735.py
@@ -0,0 +1,21 @@
+# 1735
+# 기약분수 : 분모와 분자의 공약수 = 1
+
+a,b = list(map(int,input().split(' ')))
+c,d = list(map(int,input().split(' ')))
+
+numerator = (b*c+a*d) #분자
+denominator = b*d #분모
+
+#최대공약수
+def GCD(x,y):
+    while y:
+        x,y = y,x%y
+    return x
+
+gcd = GCD(numerator,denominator)
+
+numerator = int(numerator/gcd) #최대공약수로 나누기(약분)
+denominator = int(denominator/gcd) #최대공약수로 나누기(약분)
+
+print(f"{numerator} {denominator}") #정답
